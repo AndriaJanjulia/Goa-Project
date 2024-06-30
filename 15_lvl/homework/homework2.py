5f761d1a39d69785a6f1639bf58fef2ad6923934
AndriaJanjulia <andriajanjulia8@gmail.com>
2024-06-30 17:47:22 +0400

    goa

---
+++ 15_lvl/homework/homework2.py
@@ -0,0 +1,17 @@
+steps_taken = 1000 
+time_elapsed = 10
+
+steps = int(input("Enter the number of steps taken: "))
+time = int(input("Enter the time elapsed in minures: "))
+
+print(steps == steps_taken and time == time_elapsed)
+
+
+
+steps_taken = 1000
+time_elapsed = 10
+
+steps = int(input("Enter the number of steps taken: "))
+time = int(input("Enter the time elapsed in minures: "))
+
+print(steps == steps_taken or time == time_elapsed)
