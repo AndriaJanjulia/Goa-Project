8fe1264b37887775a362be775ddf501b1771fa74
AndriaJanjulia <andriajanjulia8@gmail.com>
2024-07-10 21:39:02 +0400

    goa

---
+++ 17_lvl/classwork/classwork6.py
@@ -0,0 +1,9 @@
+GPA =int(input("please enter your gpa: "))
+Family =int(input("please enter your family early salary: "))
+
+if GPA > 3 and Family < 50000:
+    print("მიიღეთ სტიპენდია")
+
+else:
+    print("ვერ მიიღე გათიშული ოსკარი")
+
