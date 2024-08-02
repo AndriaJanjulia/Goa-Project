2caac24d2d92f99a06b215ab0fa934e8e7d9380d
AndriaJanjulia <andriajanjulia8@gmail.com>
2024-08-02 14:59:38 +0400

    goa

---
+++ 24_lvl/homework/homework.py
@@ -0,0 +1,38 @@
+for i in range(10): 
+    print(i) 
+
+print("-----------------------------------")
+
+for i in range(10, 0, -1):          
+    print(i)
+print("-----------------------------------")
+
+for i in range(158):
+    print(i)
+
+print("-----------------------------------")
+
+total = 0
+for i in range(1, 10):
+    total = total+i
+print(total)
+
+print("-----------------------------------")
+
+for i in range(50, 10, -4):
+    print(i)
+
+print("-----------------------------------")
+
+number = 1
+while number<=10:
+    print(number)
+    number = number+1
+
+print("-----------------------------------")
+
+number = 1
+while number<=150:
+    print(number)
+    number = number+15
+
