3d7b6b7ed6637ef3e04ea3f48f4a0bb86b826594
AndriaJanjulia <andriajanjulia8@gmail.com>
2024-12-12 18:25:26 +0400

    goa

--- 13_lvl/homework/homework3.py
+++ 13_lvl/homework/homework3.py
@@ -1,4 +1,1 @@
-name = input("Enter ur name: " )
-last_name =input("Enter ur last name: ")
-country = input("Which country do u live: ")
-fav_car = input("Enter ur fav car: ")
+name = input("enter your name: ")
