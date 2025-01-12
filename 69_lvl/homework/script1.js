331027004f2ec69711cd3e295b1a955c247d27fb
AndriaJanjulia <andriajanjulia8@gmail.com>
2025-01-12 16:30:34 +0400

    goa

---
+++ 69_lvl/homework/script1.js
@@ -0,0 +1,4 @@
+let name1 = "გოა"
+let name2 = "მაგარია"
+alert(name1 + ' ' +  name2)
+console.log(name1 + ' ' + name2)
---
+++ 70_lvl/classwork/script.js
@@ -0,0 +1,6 @@
+let name = "Andria"
+let lastname = "Janjulia"
+let age = 13
+console.log(name + lastname + age)
+
+alert(name + lastname + age)
