cea310b9dea04bd7085fb83cfc3aadef46e788df
AndriaJanjulia <andriajanjulia8@gmail.com>
2025-01-21 22:22:33 +0400

    goa

---
+++ 74_lvl/homework/script.js
@@ -0,0 +1,19 @@
+let goa = prompt('Enter your coffe choice: ')
+switch (goa){
+
+    case '1':
+     console.log('Americano');
+    break;
+
+    case '2':
+     console.log('Espresso')
+    break;
+
+   case '3':
+    console.log('Number is Cappuccino')
+    break;
+
+default: {
+console.log('invalid')
+}
+}
